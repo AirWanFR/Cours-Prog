d1692eff486314448f0eb8910a0ab3f6f792cc26
Erwan Combourieu <erwan.combourieu@gmail.com>
2026-02-02 23:41:18 +0100

    Exercice 1 : Finish (really that time)
    Bug with the first window (showing a previous version)

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,23 +23,107 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.No;
+            pictureBox1.Image = Properties.Resources.favicon;
+            pictureBox1.Location = new Point(529, 149);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(219, 140);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.epita_fr;
+            pictureBox2.Location = new Point(23, 149);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(242, 140);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(257, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(286, 69);
+            label1.TabIndex = 2;
+            label1.Text = "Exercice 1 ";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = Color.Coral;
+            button1.Cursor = Cursors.SizeWE;
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(334, 149);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 53);
+            button1.TabIndex = 3;
+            button1.Text = "Swap Images";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.BackColor = Color.LightSalmon;
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(321, 235);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 54);
+            button2.TabIndex = 4;
+            button2.Text = "Change Window";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(630, 360);
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            ForeColor = SystemColors.MenuText;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "Form1";
-            Text = "Coucou";
+            Text = "Exercice 1 - Bases";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Button button1;
+        private Button button2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,6 +13,20 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tmp = pictureBox1.Image;
+            pictureBox1.Image = pictureBox2.Image;
+            pictureBox2.Image = tmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 wind2 = new Form2();
+            wind2.Show();
+            this.Hide();
         }
     }
 }
